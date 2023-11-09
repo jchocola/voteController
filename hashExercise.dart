07be6e2f897e04e22a33131836da0577c5ae1837
Bak Nguen <sangsangden@gmail.com>
2023-11-09 15:49:27 +0700

    fun app

---
+++ hashExercise.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  String voterName = "";
+  bool choice;
+  Map<String, bool> myMap = {};
+  while (voterName.toLowerCase() != "stop") {
+    print("new voter");
+    print("Name:");
+    voterName = stdin.readLineSync()!;
+    if (myMap.containsKey(voterName)) {
+      print("posel von");
+    } else {
+      print("thanks for vote , enter your choise (true/false)");
+      var ch = stdin.readLineSync()!;
+      choice = parseBool(ch);
+      myMap.addAll({voterName: choice});
+    }
+  }
+  print(myMap);
+}
+
+bool parseBool(String str) {
+  if (str.toLowerCase() == 'true')
+    return true;
+  else if (str.toLowerCase() == 'false')
+    return false;
+  else
+    return false;
+}
